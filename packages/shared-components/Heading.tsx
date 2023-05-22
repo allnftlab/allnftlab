b656f76cd87495928331f61b282b6373fdfcf3c3
João Vitor F Jardim <61571174+jvfontouraj@users.noreply.github.com>
2023-05-22 11:54:35 -0300

    feat(shared-components): create Text, Heading, Button and Tailwind Color Shceme components

---
+++ packages/shared-components/Heading.tsx
@@ -0,0 +1,53 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import React from 'react'
+import cs from 'classnames'
+
+export const Headings = cva('title', {
+  variants: {
+    size: {
+      xs: ['text-base', 'leading-normal', 'md:text-lg', 'md:leading-normal'],
+      sm: ['text-xl', 'leading-normal'],
+      md: ['text-xl', 'leading-tight', 'md:text-2xl', 'md:leading-tight'],
+      lg: ['text-3xl', 'leading-tight'],
+      xl: ['text-4xl', 'leading-tight'],
+      '2xl': ['text-lg', 'leading-tight', 'md:text-5xl', 'md:leading-tight'],
+      '3xl': [
+        'text-4xl',
+        'leading-none',
+        'md:text-6xl',
+        'md:leading-none',
+        'tracking-tighter',
+      ],
+      '4xl': [
+        'text-4xl',
+        'leading-none',
+        'md:text-7xl',
+        'md:leading-none',
+        'tracking-tighter',
+      ],
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export interface TitleProps
+  extends React.HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof Headings> {}
+
+export const Heading: React.FC<TitleProps> = ({
+  className,
+  size,
+  ...props
+}) => (
+  <h2
+    className={cs(
+      'm-0',
+      'text-neutral-100',
+      'font-semibold',
+      Headings({ size, className }),
+    )}
+    {...props}
+  />
+)
